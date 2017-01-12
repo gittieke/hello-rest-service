f5c317ab25027c33e372cc02e3ea7a508a13a824
Jonas Devries <jonas@good-it.be>
2017-01-12 16:47:57 +0100

    Finetune StockQuoteController: add json resource via classpath

--- src/main/java/hello/StockQuoteController.java
+++ src/main/java/hello/StockQuoteController.java
@@ -1,28 +1,28 @@
 package hello;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StockQuoteController {
 		
-	@RequestMapping(value="/stockQuoteList", method = RequestMethod.GET, produces = "application/json")
-	@ResponseBody
-	public String stockQuoteList() throws IOException {				
-		String stockQuoteListJson = "C:\\Users\\id809201\\dev\\hello\\src\\main\\resources\\stock-quote-list.json";
-		return new String(Files.readAllBytes(Paths.get(stockQuoteListJson)));				
+	@Value("classpath:${file.json.stock.quote.yql}")
+	private Resource jsonStockQuoteYQL;
+			
+	@RequestMapping(value="/stockQuoteYQL", method=RequestMethod.GET, produces="application/json")	
+	public String stockQuoteList() throws IOException {
+		String jsonOutput = "";				
+		try (Scanner scanner = new Scanner(jsonStockQuoteYQL.getInputStream(), StandardCharsets.UTF_8.name())) {
+			jsonOutput += scanner.useDelimiter("\\A").next();
+	    }
+		return jsonOutput;							
 	}
 	
-	@RequestMapping(value="/stockQuoteYQL", method = RequestMethod.GET, produces = "application/json")
-	@ResponseBody
-	public String stockQuoteYQL() throws IOException {				
-		String stockQuoteListJson = "C:\\Users\\id809201\\dev\\hello\\src\\main\\resources\\stock-quote-yql.json";
-		return new String(Files.readAllBytes(Paths.get(stockQuoteListJson)));				
-	}
 }
